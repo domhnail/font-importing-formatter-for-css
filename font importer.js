60fae17cf0f4bbab48372e4b510e0b3b1287086d
domhnail <coledanodonnell@hotmail.com>
2023-12-10 00:30:04 -0400

    Boilerplate, declaring variables.

---
+++ font importer.js
@@ -0,0 +1,13 @@
+'use strict';
+const readlineSync = require('readline-sync');
+const fs = require('fs');
+
+
+function main() {
+let outputFile = "fonts.txt"; //
+const charSet = "utf-8";
+let fontName = readlineSync.question("Enter the font. ")
+let fontType = readlineSync.question("Enter the file extension. ")
+
+
+}
